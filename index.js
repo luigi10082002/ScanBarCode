1b1def9b818e855db2977baeeb56209efbe276fb
luigi10082002 <luigimatheus55@gmail.com>
2023-05-08 14:22:14 -0300

    inicio do app

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
+import ScanBarcode from './src/scanbar/index/index';
+
+import index from './src/index/index';
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(index);
